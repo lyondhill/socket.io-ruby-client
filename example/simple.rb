5cc4d368f63df32552953887d0fcbc2de9d1a690
Wise Monk <danielius@wisemonks.com>
2015-05-28 11:25:20 +0300

    Style fixes

--- example/simple.rb
+++ example/simple.rb
@@ -2,8 +2,8 @@ require 'socketIO'
 
 client = SocketIO.connect("localhost") do
   before_start do
-    on_message {|message| puts "incoming message: #{message}"}
-    on_event('news') { |data| puts data.first} # data is an array fo things.
+    on_message { |message| puts "incoming message: #{message}" }
+    on_event('news') { |data| puts data.first } # data is an array fo things.
   end
 
 end
